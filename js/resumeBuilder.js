32027237fa6cb0a76cc1d654c4ea5a1be9f68243
nebojsab <nebojsa.brnjasevic@gmail.com>
2015-12-31 10:00:39 +0100

    Initial changes.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -6,7 +6,7 @@
 
 //variable objects
 
-var bio = {
+var personal = {
     name: "Nebojsa Brnjasevic",
     role: "Web Developer",
     contacts: {
@@ -17,26 +17,32 @@ var bio = {
         www: "http://www.websurfart.com",
         location: "Serbia, Novi Banovci"
     },
-    biopic: "images/profileImg.png"
+    biopic: "images/profileImg.jpg",
+    welcome: "Welcome to my Online Resume",
+    skills: "Web Design, Graphic Design, Photoshop, Illustrator, HTML, CSS, Bootstrap, PHP (novice)"
 };
 
 
-var formattedName = HTMLheaderName.replace("%data%", bio.name);
-var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
-var formattedMobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
-var formattedEmail = HTMLemail.replace("%data%", bio.contacts.email);
-var formattedTwitter = HTMLtwitter.replace("%data%", bio.contacts.twitter);
-var formattedGithub = HTMLgithub.replace("%data%", bio.contacts.github);
-var formattedWWW = HTMLblog.replace("%data%", bio.contacts.www);
-var formattedLocation = HTMLlocation.replace("%data%", bio.contacts.location);
-var formattedPic = HTMLbioPic.replace("%data%", bio.biopic);
+var formattedName = HTMLheaderName.replace("%data%", personal.name);
+var formattedRole = HTMLheaderRole.replace("%data%", personal.role);
+var formattedMobile = HTMLmobile.replace("%data%", personal.contacts.mobile);
+var formattedEmail = HTMLemail.replace("%data%", personal.contacts.email);
+var formattedTwitter = HTMLtwitter.replace("%data%", personal.contacts.twitter);
+var formattedGithub = HTMLgithub.replace("%data%", personal.contacts.github);
+var formattedWWW = HTMLblog.replace("%data%", personal.contacts.www);
+var formattedLocation = HTMLlocation.replace("%data%", personal.contacts.location);
+var formattedPic = HTMLbioPic.replace("%data%", personal.biopic);
+var formattedWelcome = HTMLWelcomeMsg.replace("%data%", personal.welcome);
+var formattedSkills = HTMLskillsStart.replace("%data%", personal.skills);
 
-$("#header").prepend(formattedName);
-$("#header").prepend(formattedRole);
+$("#header").append(formattedName);
+$("#header").append(formattedRole);
 $("#topContacts").prepend(formattedMobile);
 $("#topContacts").prepend(formattedEmail);
 $("#topContacts").prepend(formattedTwitter);
 $("#topContacts").prepend(formattedGithub);
 $("#topContacts").prepend(formattedWWW);
 $("#topContacts").prepend(formattedLocation);
-$("#topContacts").prepend(formattedPic);
+$("#header").append(formattedPic);
+$("#header").append(formattedWelcome);
+$("#skillsH3").append(formattedSkills);
