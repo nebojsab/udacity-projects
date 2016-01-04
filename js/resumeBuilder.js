ea7508a70e70e2221d03d116f61bea0fca3e65d1
nebojsab <nebojsa.brnjasevic@gmail.com>
2016-01-04 15:03:40 +0100

    Initial changes.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -15,7 +15,7 @@ var personal = {
         twitter: "<a href='https://twitter.com/brnjasevic' target='_blank'>@brnjasevic</a>",
         github: "<a href='https://github.com/nebojsab' target='_blank'>https://github.com/nebojsab</a>",
         www: "<a href='http://www.websurfart.com' target='_blank'>http://www.websurfart.com</a>",
-        location: "<a href='https://www.google.rs/maps/place/%D0%9D%D0%BE%D0%B2%D0%B8+%D0%91%D0%B0%D0%BD%D0%BE%D0%B2%D1%86%D0%B8/@44.9465932,20.2856993,15.5z/data=!4m2!3m1!1s0x475a5e523f2991ef:0x87580219af315ef7?hl=sr' target='_blank'>Serbia, Novi Banovci</a>"
+        location: "Serbia, Novi Banovci"
     },
     biopic: "images/profileImg.jpg",
     welcome: "Welcome to my Online Resume",
